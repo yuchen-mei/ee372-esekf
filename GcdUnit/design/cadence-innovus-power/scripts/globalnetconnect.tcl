1894fd97709a676736af7ba112b13b4b2b4999a7
Priyanka Raina <praina@stanford.edu>
2020-12-04 23:33:49 -0800

    Fix power step to fix correctly stamp down vias.

--- GcdUnit/design/cadence-innovus-power/scripts/globalnetconnect.tcl
+++ GcdUnit/design/cadence-innovus-power/scripts/globalnetconnect.tcl
@@ -7,24 +7,15 @@
 #-------------------------------------------------------------------------
 # Global net connections for PG pins
 #-------------------------------------------------------------------------
-if { [ lindex [dbGet top.insts.cell.pgterms.name VDD] 0 ] != 0x0 } {
-  globalNetConnect VDD    -type pgpin -pin VDD    -inst * -verbose
-  globalNetConnect VSS    -type pgpin -pin VSS    -inst * -verbose
-}
+
+globalNetConnect VDD    -type pgpin -pin VPWR    -inst * -verbose
+globalNetConnect VSS    -type pgpin -pin VGND    -inst * -verbose
 
 # Connect VNW / VPW if any cells have these pins
 
 if { [ lindex [dbGet top.insts.cell.pgterms.name VNW] 0 ] != 0x0 } {
   globalNetConnect VDD    -type pgpin -pin VNW    -inst * -verbose
   globalNetConnect VSS    -type pgpin -pin VPW    -inst * -verbose
-}
-
-if { [ lindex [dbGet top.insts.cell.pgterms.name VPWR] 0 ] != 0x0 } {
-  globalNetConnect VDD    -type pgpin -pin VPWR    -inst * -verbose
-}
-
-if { [ lindex [dbGet top.insts.cell.pgterms.name VGND] 0 ] != 0x0 } {
-  globalNetConnect VSS    -type pgpin -pin VGND    -inst * -verbose
 }
 
 
