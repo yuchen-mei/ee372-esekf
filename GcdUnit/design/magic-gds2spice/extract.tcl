54b0ceb5f06457e12d305081b87c03f130664bb5
Priyanka Raina <praina@stanford.edu>
2020-12-15 16:56:55 -0800

    Extract SPICE from GDS using magic.

---
+++ GcdUnit/design/magic-gds2spice/extract.tcl
@@ -0,0 +1,10 @@
+gds read inputs/design_merged.gds
+load $::env(design_name)
+
+# Extract for LVS
+extract all
+ext2spice lvs
+ext2spice subcircuits off
+ext2spice -o outputs/design.extracted.spice
+
+quit
