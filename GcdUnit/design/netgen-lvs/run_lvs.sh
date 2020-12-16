eb6d411bc8178f1609dc6a96926633fc857b4612
Priyanka Raina <praina@stanford.edu>
2020-12-16 13:09:18 -0800

    Change to v2lvs command to remove PIN syntax in the generated spice netlist.

--- GcdUnit/design/netgen-lvs/run_lvs.sh
+++ GcdUnit/design/netgen-lvs/run_lvs.sh
@@ -1,4 +1,4 @@
-v2lvs -lsp inputs/adk/stdcells.spi -s inputs/adk/stdcells.spi -v inputs/design.lvs.v -o design_lvs.spice
+v2lvs -i -lsp inputs/adk/stdcells.spi -s inputs/adk/stdcells.spi -v inputs/design.lvs.v -o design_lvs.spice
 
 netgen -batch lvs "inputs/design_extracted.spice GcdUnit" "design_lvs.spice GcdUnit" adk/netgen.tcl | tee outputs/lvs_results.log  
 
