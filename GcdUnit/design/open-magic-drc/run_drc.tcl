4bbc60bd5ddc37263ce80b893cd92a8ca1cbdf86
Priyanka Raina <praina@stanford.edu>
2022-02-24 03:58:29 +0000

    Add options to run full drc.

--- GcdUnit/design/open-magic-drc/run_drc.tcl
+++ GcdUnit/design/open-magic-drc/run_drc.tcl
@@ -2,8 +2,18 @@
 gds read inputs/design_merged.gds
 load $::env(design_name)
 
+select top cell
+expand
+drc euclidean on
+drc style drc(full)
+drc check
+set drcresult [drc listall why]
+
+
 # Count number of DRC errors
 drc catchup
 drc count
 
 quit
+
+
